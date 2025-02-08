b9f374ede1456ccba0ce5fcb1b950adf2e36d389
taiseiss <sasaki.taisei.jz@tut.jp>
2025-02-08 23:03:46 +0900

    test_cases.py
    私はテストがすべての状況を網羅できるように同値分割によってテスト項目を考え、いくつものエラーを発見しました

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,31 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (21, calc(100,99))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (-1, calc(0,100))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (-1, calc(0.1,1000))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(1000,99))
 
+        def test_sample5 (self):
+                self.assertEqual (21, calc(0.1,99))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(99,0.1))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(99,1000))
+
+        def test_sample8 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(100,100))
+
+        def test_sample10 (self):
+                self.assertEqual (-1, calc(0.1,1000))
